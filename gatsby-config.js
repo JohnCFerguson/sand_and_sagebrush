68e18c56a6543d79485580848cb771b36807696f
John <johncferguson90@gmail.com>
2021-07-04 17:32:28 -0600

    updated packages to newest

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  pathPrefix: "/gatsby-material-ui-business-starter",
+  pathPrefix: "/sang_&_sagebrush",
   siteMetadata: {
-    title: "Sand and Sagebrush Salon and Spa",
+    title: "Sand & Sagebrush Salon and Spa",
     contact: {
       phone: "775 ... idk yet",
       email: "maddie@sandandsagebrush.com",
@@ -22,6 +22,7 @@ module.exports = {
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-stylus",
     "gatsby-plugin-remove-serviceworker",
+    "gatsby-plugin-netlify",
     {
       resolve: "gatsby-source-filesystem",
       options: {
